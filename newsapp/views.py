566654b39e31e369de6a6d7cae91c688c5ba7072
Gorossy <dmaldonadoa@uninorte.edu.co>
2022-05-23 00:33:25 -0500

    cambio de api

--- newsapp/views.py
+++ newsapp/views.py
@@ -4,18 +4,22 @@ import requests
 # Create your views here.
 
 def index(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=44d7392c6d00fbc65d5907678f788beb&countries=co&languages=es&limit=20&sort=published_desc')
+    #r = requests.get('http://api.mediastack.com/v1/news?access_key=44d7392c6d00fbc65d5907678f788beb&countries=co&languages=es&limit=20&sort=published_desc')
+    r = requests.get('https://newsapi.org/v2/top-headlines?'
+        'country=co&'
+        'language=es&'
+        'apiKey=5a4d363171aa476788659a53b1cc999f')
     res = r.json()
-    data = res['data']
+    data = res['articles']
     title = []
     description = []
-    image = []
+    urlToImage = []
     url = []
     for i in data:
             title.append(i['title'])
             description.append(i['description'])
-            image.append(i['image'])
+            urlToImage.append(i['urlToImage'])
             url.append(i['url'])
-    news = zip(title,description,image,url)
+    news = zip(title,description,urlToImage,url)
     context = {'news':news}
     return render(request, 'index.html', context)
